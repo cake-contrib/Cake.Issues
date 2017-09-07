df40c9002f4c8cb057873bc9f746839afa294bfe
Pascal Berger <pascal.berger@gmail.com>
2017-09-07 20:45:23 +0200

    Update Cake alias categories

--- Cake.Issues.PullRequests/PullRequestsAliasConstants.cs
+++ Cake.Issues.PullRequests/PullRequestsAliasConstants.cs
@@ -8,11 +8,11 @@
         /// <summary>
         /// Category to use for all Cake aliases creating pull request systems.
         /// </summary>
-        public const string PullRequestSystemCakeAliasCategory = "Pull Request System";
+        public const string PullRequestSystemCakeAliasCategory = "Pull Request Systems";
 
         /// <summary>
         /// Category to use for all Cake aliases providing functionality for writing issues to pull requests.
         /// </summary>
-        public const string ReportIssuesToPullRequestCakeAliasCategory = "Report Issues To Pull Requests";
+        public const string ReportIssuesToPullRequestCakeAliasCategory = "Reporting Issues To Pull Requests";
     }
 }
