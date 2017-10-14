862a2a1deaa2452af180536a241a347a98e825bd
Tim Johnson <the.tim.johnson@gmail.com>
2017-10-14 15:42:09 -0400

    Modify tests that rely on checking invalid path chars, so that they use an char that is invalid in both windows & linux

--- src/Cake.Issues.Tests/IssueOfTTests.cs
+++ src/Cake.Issues.Tests/IssueOfTTests.cs
@@ -10,7 +10,7 @@
         public sealed class TheCtor
         {
             [Theory]
-            [InlineData(@"foo<bar")]
+            [InlineData("foo\tbar")]
             public void Should_Throw_If_File_Path_Is_Invalid(string filePath)
             {
                 // Given / When
@@ -220,7 +220,7 @@
         public sealed class TheIssueOfTCtorWithRuleId
         {
             [Theory]
-            [InlineData(@"foo<bar")]
+            [InlineData("foo\tbar")]
             public void Should_Throw_If_File_Path_Is_Invalid(string filePath)
             {
                 // Given / When
