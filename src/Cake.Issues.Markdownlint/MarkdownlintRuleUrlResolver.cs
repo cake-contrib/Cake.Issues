b3a401152201dfb35d5549df4b6a4d90df900560
Pascal Berger <pascal.berger@gmail.com>
2018-07-27 23:18:00 +0200

    (GH-27) Add alias for custom rule url resolver registration

--- src/Cake.Issues.Markdownlint/MarkdownlintRuleUrlResolver.cs
+++ src/Cake.Issues.Markdownlint/MarkdownlintRuleUrlResolver.cs
@@ -1,11 +1,12 @@
 ﻿namespace Cake.Issues.Markdownlint
 {
     using System;
+    using System.Text.RegularExpressions;
 
     /// <summary>
     /// Class for retrieving an URL linking to a site describing a rule.
     /// </summary>
-    internal class MarkdownlintRuleUrlResolver : BaseRuleUrlResolver<BaseRuleDescription>
+    internal class MarkdownlintRuleUrlResolver : BaseRuleUrlResolver<MarkdownlintRuleDescription>
     {
         private static readonly Lazy<MarkdownlintRuleUrlResolver> InstanceValue =
             new Lazy<MarkdownlintRuleUrlResolver>(() => new MarkdownlintRuleUrlResolver());
@@ -25,8 +26,30 @@
         public static MarkdownlintRuleUrlResolver Instance => InstanceValue.Value;
 
         /// <inheritdoc/>
-        protected override bool TryGetRuleDescription(string rule, BaseRuleDescription ruleDescription)
+        protected override bool TryGetRuleDescription(string rule, MarkdownlintRuleDescription ruleDescription)
         {
+            var regex = new Regex(@"^MD(\d*)$");
+            var match = regex.Match(rule);
+
+            if (!match.Success)
+            {
+                return false;
+            }
+
+            var groups = match.Groups;
+
+            if (groups.Count != 2)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(groups[1].Value, out int ruleId))
+            {
+                return false;
+            }
+
+            ruleDescription.RuleId = ruleId;
+
             return true;
         }
     }
