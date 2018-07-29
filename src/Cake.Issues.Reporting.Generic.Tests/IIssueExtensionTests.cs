95a9eb3b5e4cd9cf4209c8e8287d2caf766df148
Pascal Berger <pascal.berger@gmail.com>
2018-07-29 21:04:56 +0200

    Add missing tests for IIssueExtension

--- src/Cake.Issues.Reporting.Generic.Tests/IIssueExtensionTests.cs
+++ src/Cake.Issues.Reporting.Generic.Tests/IIssueExtensionTests.cs
@@ -105,5 +105,105 @@
                 result.ShouldBeNull();
             }
         }
+
+        public sealed class TheFileDirectoryExtension
+        {
+            [Fact]
+            public void Should_Throw_If_Issue_Is_Null()
+            {
+                // Given
+                IIssue issue = null;
+
+                // When
+                var result = Record.Exception(() => issue.FileDirectory());
+
+                // Then
+                result.IsArgumentNullException("issue");
+            }
+
+            [Fact]
+            public void Should_Return_Full_Path()
+            {
+                // Given
+                var filePath = @"src\Cake.Issues.Reporting.Generic.Tests\Foo.cs";
+                var issue =
+                    IssueBuilder
+                        .NewIssue("Message Foo", "ProviderType Foo", "ProviderName Foo")
+                        .InFile(filePath)
+                        .Create();
+
+                // When
+                var result = issue.FileDirectory();
+
+                // Then
+                result.ShouldBe(@"src/Cake.Issues.Reporting.Generic.Tests");
+            }
+
+            [Fact]
+            public void Should_Return_Null_If_File_Is_Not_Set()
+            {
+                // Given
+                var issue =
+                    IssueBuilder
+                        .NewIssue("Message Foo", "ProviderType Foo", "ProviderName Foo")
+                        .Create();
+
+                // When
+                var result = issue.FileDirectory();
+
+                // Then
+                result.ShouldBeNull();
+            }
+        }
+
+        public sealed class TheFileNameExtension
+        {
+            [Fact]
+            public void Should_Throw_If_Issue_Is_Null()
+            {
+                // Given
+                IIssue issue = null;
+
+                // When
+                var result = Record.Exception(() => issue.FileName());
+
+                // Then
+                result.IsArgumentNullException("issue");
+            }
+
+            [Fact]
+            public void Should_Return_Full_Path()
+            {
+                // Given
+                var filePath = @"src\Cake.Issues.Reporting.Generic.Tests\Foo.cs";
+                var issue =
+                    IssueBuilder
+                        .NewIssue("Message Foo", "ProviderType Foo", "ProviderName Foo")
+                        .InFile(filePath)
+                        .Create();
+
+                // When
+                var result = issue.FileName();
+
+                // Then
+                result.ShouldBe("Foo.cs");
+            }
+
+            [Fact]
+            public void Should_Return_Null_If_File_Is_Not_Set()
+            {
+                // Given
+                var issue =
+                    IssueBuilder
+                        .NewIssue("Message Foo", "ProviderType Foo", "ProviderName Foo")
+                        .Create();
+
+                // When
+                var result = issue.FileName();
+
+                // Then
+                result.ShouldBeNull();
+            }
+        }
     }
 }
