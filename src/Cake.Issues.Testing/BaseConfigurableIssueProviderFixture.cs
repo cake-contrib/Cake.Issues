8b9b7981e5f7ba8940ed8813cdf1e8fb30b30e4e
Pascal Berger <pascal.berger@gmail.com>
2018-08-05 16:21:55 +0200

    Add base text fixtures for issue providers

---
+++ src/Cake.Issues.Testing/BaseConfigurableIssueProviderFixture.cs
@@ -0,0 +1,75 @@
+﻿namespace Cake.Issues.Testing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// Base class for test fixtures for testing issue providers inherited from <see cref="BaseConfigurableIssueProvider{T}"/>.
+    /// </summary>
+    /// <typeparam name="TIssueProvider">Type of issue provider.</typeparam>
+    /// <typeparam name="TSettings">Type of the settings for the issue provider.</typeparam>
+    public abstract class BaseConfigurableIssueProviderFixture<TIssueProvider, TSettings> : BaseIssueProviderFixture<TIssueProvider>
+        where TIssueProvider : BaseConfigurableIssueProvider<TSettings>
+        where TSettings : IssueProviderSettings
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseConfigurableIssueProviderFixture{TIssueProvider, TSettings}"/> class.
+        /// </summary>
+        /// <param name="fileResourceName">Name of the resource to load.</param>
+        protected BaseConfigurableIssueProviderFixture(string fileResourceName)
+        {
+            using (var ms = new MemoryStream())
+            using (var stream = this.GetType().Assembly.GetManifestResourceStream(this.FileResourceNamespace + fileResourceName))
+            {
+                stream.CopyTo(ms);
+                this.LogFileContent = ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the content of the log file.
+        /// </summary>
+        public byte[] LogFileContent { get; set; }
+
+        /// <summary>
+        /// Gets the namespace where test file resources will be searched.
+        /// </summary>
+        protected abstract string FileResourceNamespace { get; }
+
+        /// <inheritdoc/>
+        protected override IList<object> GetCreateIssueProviderArguments()
+        {
+            var result = base.GetCreateIssueProviderArguments();
+            result.Add(this.CreateIssueProviderSettings());
+            return result;
+        }
+
+        /// <summary>
+        /// Creates a new instance of the issue provider settings.
+        /// </summary>
+        /// <returns>Instance of the issue provider settings.</returns>
+        protected virtual IList<object> GetCreateIssueProviderSettingsArguments()
+        {
+            if (this.LogFileContent == null || !this.LogFileContent.Any())
+            {
+                throw new InvalidOperationException("No log content set.");
+            }
+
+            return new List<object> { this.LogFileContent };
+        }
+
+        /// <summary>
+        /// Creates a new instance of the issue provider settings.
+        /// </summary>
+        /// <returns>Instance of the issue provider.</returns>
+        private TSettings CreateIssueProviderSettings()
+        {
+            return
+                (TSettings)Activator.CreateInstance(
+                    typeof(TSettings),
+                    this.GetCreateIssueProviderSettingsArguments().ToArray());
+        }
+    }
+}
---
+++ src/Cake.Issues.Testing/BaseMultiFormatIssueProviderFixture.cs
@@ -0,0 +1,35 @@
+﻿namespace Cake.Issues.Testing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Base class for test fixtures for testing issue providers inherited from <see cref="BaseMultiFormatIssueProvider{TSettings, TIssueProvider}"/>.
+    /// </summary>
+    /// <typeparam name="TIssueProvider">Type of issue provider.</typeparam>
+    /// <typeparam name="TSettings">Type of the settings for the issue provider.</typeparam>
+    /// <typeparam name="TLogFileFormat">Type of the log file format.</typeparam>
+    public abstract class BaseMultiFormatIssueProviderFixture<TIssueProvider, TSettings, TLogFileFormat> : BaseConfigurableIssueProviderFixture<TIssueProvider, TSettings>
+        where TIssueProvider : BaseMultiFormatIssueProvider<TSettings, TIssueProvider>
+        where TSettings : BaseMultiFormatIssueProviderSettings<TIssueProvider, TSettings>
+        where TLogFileFormat : ILogFileFormat<TIssueProvider, TSettings>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseMultiFormatIssueProviderFixture{TIssueProvider, TSettings, TLogFileFormat}"/> class.
+        /// </summary>
+        /// <param name="fileResourceName">Name of the resource to load.</param>
+        public BaseMultiFormatIssueProviderFixture(string fileResourceName)
+            : base(fileResourceName)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override IList<object> GetCreateIssueProviderSettingsArguments()
+        {
+            var result = base.GetCreateIssueProviderSettingsArguments();
+            result.Add((TLogFileFormat)Activator.CreateInstance(typeof(TLogFileFormat), this.Log));
+            return result;
+        }
+    }
+}
