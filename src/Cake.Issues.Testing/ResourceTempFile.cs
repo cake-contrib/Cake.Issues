84621fa80cd33b55195e486fe6d4c829425f5023
Pascal Berger <pascal.berger@gmail.com>
2018-08-06 14:34:03 +0200

    (GH-47) Add helper for working with embedded files on disk

---
+++ src/Cake.Issues.Testing/ResourceTempFile.cs
@@ -0,0 +1,53 @@
+﻿namespace Cake.Issues.Testing
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    /// <summary>
+    /// Creates a temporary file from an embedded resource.
+    /// </summary>
+    public sealed class ResourceTempFile : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceTempFile"/> class.
+        /// </summary>
+        /// <param name="resourceName">Full name of the embedded resource in the calling assembly.</param>
+        public ResourceTempFile(string resourceName)
+        {
+            resourceName.NotNullOrWhiteSpace(nameof(resourceName));
+
+            using (var ms = new MemoryStream())
+            using (var stream = Assembly.GetCallingAssembly().GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    throw new ArgumentException(
+                        $"Could not find resource {resourceName}", nameof(resourceName));
+                }
+
+                stream.CopyTo(ms);
+                var data = ms.ToArray();
+
+                using (var file = new FileStream(this.FileName, FileMode.Create, FileAccess.Write))
+                {
+                    file.Write(data, 0, data.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the temporary file.
+        /// </summary>
+        public string FileName { get; } = Path.GetTempFileName();
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (File.Exists(this.FileName))
+            {
+                File.Delete(this.FileName);
+            }
+        }
+    }
+}
---
+++ src/Cake.Issues.Tests/Testing/ResourceTempFileTests.cs
@@ -0,0 +1,100 @@
+﻿namespace Cake.Issues.Tests.Testing
+{
+    using System.IO;
+    using Cake.Issues.Testing;
+    using Shouldly;
+    using Xunit;
+
+    public sealed class ResourceTempFileTests
+    {
+        public sealed class TheCtor
+        {
+            [Fact]
+            public void Should_Throw_If_ResourceName_Is_Null()
+            {
+                // Given
+                string resourceName = null;
+
+                // When
+                var result = Record.Exception(() => new ResourceTempFile(resourceName));
+
+                // Then
+                result.IsArgumentNullException("resourceName");
+            }
+
+            [Fact]
+            public void Should_Throw_If_ResourceName_Is_Empty()
+            {
+                // Given
+                var resourceName = string.Empty;
+
+                // When
+                var result = Record.Exception(() => new ResourceTempFile(resourceName));
+
+                // Then
+                result.IsArgumentOutOfRangeException("resourceName");
+            }
+
+            [Fact]
+            public void Should_Throw_If_ResourceName_Is_WhiteSpace()
+            {
+                // Given
+                var resourceName = " ";
+
+                // When
+                var result = Record.Exception(() => new ResourceTempFile(resourceName));
+
+                // Then
+                result.IsArgumentOutOfRangeException("resourceName");
+            }
+
+            [Fact]
+            public void Should_Throw_If_ResourceName_Does_Not_Exist()
+            {
+                // Given
+                var resourceName = "foo";
+
+                // When
+                var result = Record.Exception(() => new ResourceTempFile(resourceName));
+
+                // Then
+                result.IsArgumentException("resourceName");
+            }
+
+            [Fact]
+            public void Should_Create_Temp_File()
+            {
+                // Given
+                var resourceName = "Cake.Issues.Tests.Testfiles.Build.log";
+
+                // When
+                using (var tempFile = new ResourceTempFile(resourceName))
+                {
+                    // Then
+                    File.Exists(tempFile.FileName).ShouldBe(true);
+                }
+            }
+
+        }
+
+        public sealed class TheDisposeMethod
+        {
+            [Fact]
+            public void Should_Remove_Temp_File()
+            {
+                // Given
+                var resourceName = "Cake.Issues.Tests.Testfiles.Build.log";
+                string tempFileName;
+
+                // When
+                using (var tempFile = new ResourceTempFile(resourceName))
+                {
+                    tempFileName = tempFile.FileName;
+                }
+
+                // Then
+                File.Exists(tempFileName).ShouldBe(false);
+            }
+        }
+    }
+}
