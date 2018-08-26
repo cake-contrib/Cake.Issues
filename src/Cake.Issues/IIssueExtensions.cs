0b322268a6a52d7e8b2de966c2e7ca85197cb8dc
Pascal Berger <pascal.berger@gmail.com>
2018-08-26 18:35:13 +0200

    (GH-57) Add extension for replacing issue tokens in string

--- src/Cake.Issues/IIssueExtensions.cs
+++ src/Cake.Issues/IIssueExtensions.cs
@@ -64,5 +64,98 @@
 
             return issue.AffectedFileRelativePath?.GetFilename().ToString();
         }
+
+        /// <summary>
+        /// Returns a string with all patterns replaced by the values of <paramref name="issue"/>.
+        /// </summary>
+        /// <param name="pattern">Pattern whose values should be replaced.
+        /// The following patterns are supported:
+        /// <list type="table">
+        ///     <listheader>
+        ///         <term>Pattern</term>
+        ///         <description>Description</description>
+        ///     </listheader>
+        ///     <item>
+        ///         <term>{ProviderType}</term>
+        ///         <description>The value of <see cref="IIssue.ProviderType"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{ProviderName}</term>
+        ///         <description>The value of <see cref="IIssue.ProviderName"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{Priority}</term>
+        ///         <description>The value of <see cref="IIssue.Priority"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{PriorityName}</term>
+        ///         <description>The value of <see cref="IIssue.PriorityName"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{ProjectPath}</term>
+        ///         <description>The value of <see cref="ProjectPath(IIssue)"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{ProjectDirectory}</term>
+        ///         <description>The value of <see cref="ProjectDirectory(IIssue)"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{ProjectName}</term>
+        ///         <description>The value of <see cref="IIssue.ProjectName"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{FilePath}</term>
+        ///         <description>The value of <see cref="FilePath(IIssue)"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{FileDirectory}</term>
+        ///         <description>The value of <see cref="FileDirectory(IIssue)"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{FileName}</term>
+        ///         <description>The value of <see cref="FileName(IIssue)"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{Line}</term>
+        ///         <description>The value of <see cref="IIssue.Line"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{Rule}</term>
+        ///         <description>The value of <see cref="IIssue.Rule"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{RuleUrl}</term>
+        ///         <description>The value of <see cref="IIssue.RuleUrl"/>.</description>
+        ///     </item>
+        ///     <item>
+        ///         <term>{Message}</term>
+        ///         <description>The value of <see cref="IIssue.Message"/>.</description>
+        ///     </item>
+        /// </list>
+        /// </param>
+        /// <param name="issue">Issue whose values should be used to replace the patterns.</param>
+        /// <returns>Value with all patterns replaced.</returns>
+        public static string ReplaceIssuePattern(this string pattern, IIssue issue)
+        {
+            pattern.NotNull(nameof(pattern));
+            issue.NotNull(nameof(issue));
+
+            return
+                pattern
+                    .Replace("{ProviderType}", issue.ProviderType)
+                    .Replace("{ProviderName}", issue.ProviderName)
+                    .Replace("{Priority}", issue.Priority?.ToString())
+                    .Replace("{PriorityName}", issue.PriorityName)
+                    .Replace("{ProjectPath}", issue.ProjectPath())
+                    .Replace("{ProjectDirectory}", issue.ProjectDirectory())
+                    .Replace("{ProjectName}", issue.ProjectName)
+                    .Replace("{FilePath}", issue.FilePath())
+                    .Replace("{FileDirectory}", issue.FileDirectory())
+                    .Replace("{FileName}", issue.FileName())
+                    .Replace("{Line}", issue.Line?.ToString())
+                    .Replace("{Rule}", issue.Rule)
+                    .Replace("{RuleUrl}", issue.RuleUrl?.ToString())
+                    .Replace("{Message}", issue.Message);
+        }
     }
 }
