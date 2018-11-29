d1762c7726492477dcb33882d52e4615533b9b23
Vadim Hatsura <vadimgatsura@gmail.com>
2018-11-29 23:41:32 +0300

    Migrate to netstandard2.0

--- Cake.Issues.Reporting.Tests/Properties/AssemblyInfo.cs
+++ Cake.Issues.Reporting.Tests/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Cake.Issues.Reporting.Tests")]
-[assembly: AssemblyDescription("Tests for the Cake.Issues.Reporting add-in")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BBT Software AG")]
-[assembly: AssemblyProduct("Cake.Issues")]
-[assembly: AssemblyCopyright("Copyright © BBT Software AG and contributors")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6f69c49c-0410-411e-b4f1-57db5ce5dfb9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
