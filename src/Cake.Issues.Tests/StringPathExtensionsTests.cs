b50d58591bf2144762933d85962461aea5ef2530
Pascal Berger <pascal.berger@gmail.com>
2019-07-13 15:42:22 +0200

    Fix test cases on Linux

--- src/Cake.Issues.Tests/StringPathExtensionsTests.cs
+++ src/Cake.Issues.Tests/StringPathExtensionsTests.cs
@@ -61,7 +61,7 @@
             }
 
             [Theory]
-            [InlineData("foo\tbar")]
+            [InlineData("foo\0bar")]
             public void Should_Return_False_If_Invalid_Path(string path)
             {
                 // Given / When
@@ -108,7 +108,7 @@
             public void Should_Throw_If_Path_Is_Not_Valid()
             {
                 // Given / When
-                var result = Record.Exception(() => "c:\\foo\tbar".IsFullPath());
+                var result = Record.Exception(() => "c:\\foo\0bar".IsFullPath());
 
                 // Then
                 result.IsArgumentException("path");
@@ -183,7 +183,7 @@
             public void Should_Throw_If_Path_Is_Invalid()
             {
                 // Given / When
-                var result = Record.Exception(() => "c:\\foo\tbar".IsSubpathOf(@"c:\foo"));
+                var result = Record.Exception(() => "c:\\foo\0bar".IsSubpathOf(@"c:\foo"));
 
                 // Then
                 result.IsArgumentException("path");
@@ -223,7 +223,7 @@
             public void Should_Throw_If_BaseDirPath_Is_Invalid()
             {
                 // Given / When
-                var result = Record.Exception(() => @"c:\foo\bar".IsSubpathOf("c:\\f\to"));
+                var result = Record.Exception(() => @"c:\foo\bar".IsSubpathOf("c:\\f\0o"));
 
                 // Then
                 result.IsArgumentException("baseDirPath");
@@ -361,7 +361,7 @@
             public void Should_Throw_If_Path_Is_Invalid()
             {
                 // Given / When
-                var result = Record.Exception(() => "c:\\foo\tbar".NormalizePath());
+                var result = Record.Exception(() => "c:\\foo\0bar".NormalizePath());
 
                 // Then
                 result.IsArgumentException("path");
