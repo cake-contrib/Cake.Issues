d04e0f7959fbdc62ba406f68ef5481ec467f80b4
Pascal Berger <pascal.berger@gmail.com>
2019-09-30 21:52:22 +0200

    (GH-133) Add versioning to serialization format of issues

---
+++ src/Cake.Issues.Tests/Serialization/IssueDeserializationExtensionsTests.cs
@@ -0,0 +1,170 @@
+﻿namespace Cake.Issues.Tests.Serialization
+{
+    using System;
+    using System.Linq;
+    using Cake.Core.IO;
+    using Cake.Issues.Serialization;
+    using Cake.Issues.Testing;
+    using Shouldly;
+    using Xunit;
+
+    public sealed class IssueDeserializationExtensionsTests
+    {
+        public sealed class TheDeserializeToIssueExtensionForAJsonString
+        {
+            [Fact]
+            public void Should_Throw_If_JsonString_Is_Null()
+            {
+                // Given
+                string jsonString = null;
+
+                // When
+                var result = Record.Exception(() => jsonString.DeserializeToIssue());
+
+                // Then
+                result.IsArgumentNullException("jsonString");
+            }
+
+            [Fact]
+            public void Should_Throw_If_JsonString_Has_Unknown_Version()
+            {
+                // Given
+                var jsonString = "{\"Version\": -1}";
+
+                // When
+                var result = Record.Exception(() => jsonString.DeserializeToIssue());
+
+                // Then
+                result.Message.ShouldBe("Not supported issue serialization format -1");
+            }
+        }
+
+        public sealed class TheDeserializeToIssuesExtensionForAJsonString
+        {
+            [Fact]
+            public void Should_Throw_If_JsonString_Is_Null()
+            {
+                // Given
+                string jsonString = null;
+
+                // When
+                var result = Record.Exception(() => jsonString.DeserializeToIssues());
+
+                // Then
+                result.IsArgumentNullException("jsonString");
+            }
+
+            [Fact]
+            public void Should_Return_An_Empty_List_For_An_Empty_Array()
+            {
+                // Given
+                string jsonString = "[]";
+
+                // When
+                var result = jsonString.DeserializeToIssues();
+
+                // Then
+                result.ShouldBeEmpty();
+            }
+        }
+
+        public sealed class TheDeserializeToIssueExtensionForAJsonFile
+        {
+            [Fact]
+            public void Should_Throw_If_FilePath_Is_Null()
+            {
+                // Given
+                FilePath filePath = null;
+
+                // When
+                var result = Record.Exception(() => filePath.DeserializeToIssue());
+
+                // Then
+                result.IsArgumentNullException("filePath");
+            }
+
+            [Fact]
+            public void Should_Return_Issue()
+            {
+                // Given
+                var filePath = new FilePath("Testfiles/issue.json");
+
+                // When
+                var result = filePath.DeserializeToIssue();
+
+                // Then
+                IssueChecker.Check(
+                    result,
+                    IssueBuilder.NewIssue(
+                        "Something went wrong.",
+                        "TestProvider",
+                        "Test Provider")
+                        .InProject(@"src\Foo\Bar.csproj", "Bar")
+                        .InFile(@"src\Foo\Bar.cs", 42)
+                        .OfRule("Rule", new Uri("https://google.com"))
+                        .WithPriority(IssuePriority.Warning));
+            }
+        }
+
+        public sealed class TheDeserializeToIssuesExtensionForAJsonFile
+        {
+            [Fact]
+            public void Should_Throw_If_FilePath_Is_Null()
+            {
+                // Given
+                FilePath filePath = null;
+
+                // When
+                var result = Record.Exception(() => filePath.DeserializeToIssues());
+
+                // Then
+                result.IsArgumentNullException("filePath");
+            }
+
+            [Fact]
+            public void Should_Return_An_Empty_List_For_An_Empty_Array()
+            {
+                // Given
+                var filePath = new FilePath("Testfiles/empty-array.json");
+
+                // When
+                var result = filePath.DeserializeToIssues();
+
+                // Then
+                result.ShouldBeEmpty();
+            }
+
+            [Fact]
+            public void Should_Return_List_Of_Issues()
+            {
+                // Given
+                var filePath = new FilePath("Testfiles/issues.json");
+
+                // When
+                var result = filePath.DeserializeToIssues().ToList();
+
+                // Then
+                result.Count.ShouldBe(2);
+                IssueChecker.Check(
+                    result[0],
+                    IssueBuilder.NewIssue(
+                        "Something went wrong.",
+                        "TestProvider",
+                        "Test Provider")
+                        .InProject(@"src\Foo\Bar.csproj", "Bar")
+                        .InFile(@"src\Foo\Bar.cs", 42)
+                        .OfRule("Rule", new Uri("https://google.com"))
+                        .WithPriority(IssuePriority.Warning));
+                IssueChecker.Check(
+                    result[1],
+                    IssueBuilder.NewIssue(
+                        "Something went wrong again.",
+                        "TestProvider",
+                        "Test Provider")
+                        .InProject(@"src\Foo\Bar.csproj", "Bar")
+                        .InFile(@"src\Foo\Bar2.cs")
+                        .WithPriority(IssuePriority.Warning));
+            }
+        }
+    }
+}
---
+++ src/Cake.Issues/Serialization/IssueDeserializationExtensions.cs
@@ -0,0 +1,139 @@
+﻿namespace Cake.Issues.Serialization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using Cake.Core.IO;
+    using LitJson;
+
+    /// <summary>
+    /// Extensions for deserializing an <see cref="IIssue"/>.
+    /// </summary>
+    internal static class IssueDeserializationExtensions
+    {
+        /// <summary>
+        /// Deserializes an <see cref="Issue"/> from a JSON string.
+        /// </summary>
+        /// <param name="jsonString">JSON representation of the issue.</param>
+        /// <returns>Instance of the issue.</returns>
+        public static Issue DeserializeToIssue(this string jsonString)
+        {
+            jsonString.NotNullOrWhiteSpace(nameof(jsonString));
+
+            using (var stream = new MemoryStream(Encoding.Default.GetBytes(jsonString)))
+            {
+                return DeserializeStreamToIssue(stream);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes an <see cref="IEnumerable{IIssue}"/> from a JSON string.
+        /// </summary>
+        /// <param name="jsonString">JSON representation of the issues.</param>
+        /// <returns>List of issues.</returns>
+        public static IEnumerable<Issue> DeserializeToIssues(this string jsonString)
+        {
+            jsonString.NotNullOrWhiteSpace(nameof(jsonString));
+
+            using (var stream = new MemoryStream(Encoding.Default.GetBytes(jsonString)))
+            {
+                return DeserializeStreamToIssues(stream);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes an <see cref="Issue"/> from a JSON file.
+        /// </summary>
+        /// <param name="filePath">Path to the JSON file.</param>
+        /// <returns>Instance of the issue.</returns>
+        public static Issue DeserializeToIssue(this FilePath filePath)
+        {
+            filePath.NotNull(nameof(filePath));
+
+            using (var stream = File.Open(filePath.FullPath, FileMode.Open))
+            {
+                return DeserializeStreamToIssue(stream);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes an <see cref="IEnumerable{IIssue}"/> from a JSON file.
+        /// </summary>
+        /// <param name="filePath">Path to the JSON file.</param>
+        /// <returns>List of issues.</returns>
+        public static IEnumerable<Issue> DeserializeToIssues(this FilePath filePath)
+        {
+            filePath.NotNull(nameof(filePath));
+
+            using (var stream = File.Open(filePath.FullPath, FileMode.Open))
+            {
+                return DeserializeStreamToIssues(stream);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes a stream containing the JSON representation of an issue to an <see cref="Issue"/>.
+        /// </summary>
+        /// <param name="stream">Stream whose content should be deserialized.</param>
+        /// <returns>Issue instance.</returns>
+        private static Issue DeserializeStreamToIssue(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                var jsonContent = reader.ReadToEnd();
+
+                var data = JsonMapper.ToObject(jsonContent);
+
+                return DeserializeJsonDataToIssue(data);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes a stream containing the JSON representation of an array of issues to an <see cref="IEnumerable{Issue}"/>.
+        /// </summary>
+        /// <param name="stream">Stream whose content should be deserialized.</param>
+        /// <returns>List of issues.</returns>
+        private static IEnumerable<Issue> DeserializeStreamToIssues(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                var jsonContent = reader.ReadToEnd();
+
+                var data = JsonMapper.ToObject(jsonContent);
+                var issues = new List<Issue>();
+                foreach (JsonData element in data)
+                {
+                    issues.Add(DeserializeJsonDataToIssue(element));
+                }
+
+                return issues;
+            }
+        }
+
+        /// <summary>
+        /// Deserializes JSON repesentation of an issue to an <see cref="Issue"/>.
+        /// Supports serialization format of the current version of Cake.Issues as versions
+        /// written with previous versions of Cake.Issues.
+        /// </summary>
+        /// <param name="data">JSON representation of the issue.</param>
+        /// <returns>Issue instance.</returns>
+        private static Issue DeserializeJsonDataToIssue(JsonData data)
+        {
+            if (data.ContainsKey("Version"))
+            {
+                var version = (int)data["Version"];
+                switch (version)
+                {
+                    default:
+                        throw new Exception($"Not supported issue serialization format {version}");
+                }
+            }
+            else
+            {
+                // If no version is available deserialize to original format.
+                return JsonMapper.ToObject<SerializableIssue>(data.ToJson()).ToIssue();
+            }
+        }
+    }
+}
