f09151b2433325f002a931725964d40f9d1908f3
Pascal Berger <pascal.berger@gmail.com>
2020-08-12 23:20:27 +0200

    Move Cake.Issues.Reporting

---
+++ src/Cake.Issues.Reporting.Tests/IssueReportCreatorTests.cs
@@ -0,0 +1,164 @@
+﻿namespace Cake.Issues.Reporting.Tests
+{
+    using System.Collections.Generic;
+    using Cake.Issues.Testing;
+    using Shouldly;
+    using Xunit;
+
+    public sealed class IssueReportCreatorTests
+    {
+        public sealed class TheCtor
+        {
+            [Fact]
+            public void Should_Throw_If_Log_Is_Null()
+            {
+                // Given
+                var fixture = new IssueReportFormatFixture
+                {
+                    Log = null,
+                };
+
+                // When
+                var result = Record.Exception(() => fixture.CreateReport(new List<IIssue>()));
+
+                // Then
+                result.IsArgumentNullException("log");
+            }
+
+            [Fact]
+            public void Should_Throw_If_Settings_Is_Null()
+            {
+                // Given
+                var fixture = new IssueReportFormatFixture
+                {
+                    CreateIssueReportSettings = null,
+                };
+
+                // When
+                var result = Record.Exception(() => fixture.CreateReport(new List<IIssue>()));
+
+                // Then
+                result.IsArgumentNullException("settings");
+            }
+        }
+
+        public sealed class TheCreateReportForIssueProvidersMethod
+        {
+            [Fact]
+            public void Should_Initialize_Report_Format()
+            {
+                // Given
+                var fixture = new IssueReportFormatFixture();
+                var issueProviders = new List<FakeIssueProvider> { new FakeIssueProvider(fixture.Log) };
+
+                // When
+                fixture.CreateReport(issueProviders);
+
+                // Then
+                fixture.IssueReportFormat.Settings.ShouldBe(fixture.CreateIssueReportFromIssueProviderSettings);
+            }
+
+            [Fact]
+            public void Should_Return_Null_If_Initialization_Fails()
+            {
+                // Given
+                var fixture = new IssueReportFormatFixture();
+                fixture.IssueReportFormat.ShouldFailOnInitialization = true;
+                var issueProviders = new List<FakeIssueProvider> { new FakeIssueProvider(fixture.Log) };
+
+                // When
+                var result = fixture.CreateReport(issueProviders);
+
+                // Then
+                result.ShouldBeNull();
+            }
+
+            [Fact]
+            public void Should_Return_File_Path()
+            {
+                // Given
+                var fixture = new IssueReportFormatFixture();
+                var issueProviders = new List<FakeIssueProvider> { new FakeIssueProvider(fixture.Log) };
+
+                // When
+                var result = fixture.CreateReport(issueProviders);
+
+                // Then
+                result.ShouldNotBeNull();
+            }
+        }
+
+        public sealed class TheCreateReportForIssuesMethod
+        {
+            [Fact]
+            public void Should_Initialize_Report_Format()
+            {
+                // Given
+                var fixture = new IssueReportFormatFixture();
+                var issues =
+                    new List<IIssue>
+                    {
+                        IssueBuilder
+                            .NewIssue("Message", "ProviderType", "ProviderName")
+                            .InFile(@"src\Cake.Issues.Reporting\Foo.cs", 10)
+                            .OfRule("Rule")
+                            .WithPriority(IssuePriority.Warning)
+                            .Create(),
+                    };
+
+                // When
+                fixture.CreateReport(issues);
+
+                // Then
+                fixture.IssueReportFormat.Settings.ShouldBe(fixture.CreateIssueReportSettings);
+            }
+
+            [Fact]
+            public void Should_Return_Null_If_Initialization_Fails()
+            {
+                // Given
+                var fixture = new IssueReportFormatFixture();
+                fixture.IssueReportFormat.ShouldFailOnInitialization = true;
+                var issues =
+                    new List<IIssue>
+                    {
+                        IssueBuilder
+                            .NewIssue("Message", "ProviderType", "ProviderName")
+                            .InFile(@"src\Cake.Issues.Reporting\Foo.cs", 10)
+                            .OfRule("Rule")
+                            .WithPriority(IssuePriority.Warning)
+                            .Create(),
+                    };
+
+                // When
+                var result = fixture.CreateReport(issues);
+
+                // Then
+                result.ShouldBeNull();
+            }
+
+            [Fact]
+            public void Should_Return_File_Path()
+            {
+                // Given
+                var fixture = new IssueReportFormatFixture();
+                var issues =
+                    new List<IIssue>
+                    {
+                        IssueBuilder
+                            .NewIssue("Message", "ProviderType", "ProviderName")
+                            .InFile(@"src\Cake.Issues.Reporting\Foo.cs", 10)
+                            .OfRule("Rule")
+                            .WithPriority(IssuePriority.Warning)
+                            .Create(),
+                    };
+
+                // When
+                var result = fixture.CreateReport(issues);
+
+                // Then
+                result.ShouldNotBeNull();
+            }
+        }
+    }
+}
---
+++ src/Cake.Issues.Reporting/CreateIssueReportFromIssueProviderSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace Cake.Issues.Reporting
+{
+    using Cake.Core.IO;
+
+    /// <summary>
+    /// Setting affecting how reports are created which are built passing issue providers.
+    /// </summary>
+    public class CreateIssueReportFromIssueProviderSettings : ReadIssuesSettings, ICreateIssueReportFromIssueProviderSettings
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateIssueReportFromIssueProviderSettings"/> class.
+        /// </summary>
+        /// <param name="repositoryRoot">Root path of the repository.</param>
+        /// <param name="outputFilePath">Path of the generated report file.</param>
+        public CreateIssueReportFromIssueProviderSettings(DirectoryPath repositoryRoot, FilePath outputFilePath)
+            : base(repositoryRoot)
+        {
+            outputFilePath.NotNull(nameof(outputFilePath));
+
+            this.OutputFilePath = outputFilePath;
+        }
+
+        /// <inheritdoc/>
+        public FilePath OutputFilePath { get; private set; }
+    }
+}
