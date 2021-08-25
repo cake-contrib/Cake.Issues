55deaf8e7dc6232cebdd7df36878278d4cea57d2
Pascal Berger <pascal.berger@gmail.com>
2021-08-25 21:29:17 +0200

    Update Errata to 0.3.0 (#9)

--- src/Cake.Issues.Reporting.Console/ConsoleIssueReportGenerator.cs
+++ src/Cake.Issues.Reporting.Console/ConsoleIssueReportGenerator.cs
@@ -49,64 +49,15 @@
                     .Where(x => x.Line.HasValue && x.Column.HasValue)
                     .ToList();
 
-            var report = new Report();
+            var report = new Report(new FileSystemRepository(this.Settings));
             foreach (var issue in issues)
             {
-                var color = issue.Priority switch
-                {
-                    (int)IssuePriority.Error => Color.Red,
-                    (int)IssuePriority.Warning => Color.Yellow,
-                    (int)IssuePriority.Suggestion or (int)IssuePriority.Hint => Color.Blue,
-                    _ => throw new Exception(),
-                };
-
-                (var location, var length) = this.GetLocation(issue);
-                var label =
-                    new Label(
-                        issue.AffectedFileRelativePath.FullPath,
-                        location,
-                        issue.Message(IssueCommentFormat.PlainText))
-                    .WithColor(color);
-
-                if (length > 0)
-                {
-                    label = label.WithLength(length);
-                }
-
-                report.AddDiagnostic(
-                    new IssueDiagnostic(issue)
-                        .WithLabel(label));
+                report.AddDiagnostic(new IssueDiagnostic(issue));
             }
 
-            report.Render(
-                AnsiConsole.Console,
-                new FileSystemRepository(this.Settings));
+            report.Render(AnsiConsole.Console);
 
             return null;
-        }
-
-        /// <summary>
-        /// Returns the diagnostic location of an issue.
-        /// </summary>
-        /// <param name="issue">Issue for which the location should be returned.</param>
-        /// <returns>Location for the diagnostic.</returns>
-        private (Location location, int lenght) GetLocation(IIssue issue)
-        {
-            // Errata currently doesn't support file or line level diagnostics.
-            if (!issue.Line.HasValue || !issue.Column.HasValue)
-            {
-                return default;
-            }
-
-            var location = new Location(issue.Line.Value, issue.Column.Value);
-
-            int lenght = 0;
-            if (issue.EndColumn.HasValue)
-            {
-                lenght = issue.EndColumn.Value - issue.Column.Value;
-            }
-
-            return (location, lenght);
         }
     }
 }
--- src/Cake.Issues.Reporting.Console/IssueDiagnostic.cs
+++ src/Cake.Issues.Reporting.Console/IssueDiagnostic.cs
@@ -19,25 +19,72 @@
             : base(issue.Rule)
         {
             this.issue = issue;
+
+            this.Category = this.issue.PriorityName;
+            this.Color =
+                this.issue.Priority switch
+                {
+                    (int)IssuePriority.Error => Color.Red,
+                    (int)IssuePriority.Warning => Color.Yellow,
+                    (int)IssuePriority.Suggestion => Color.Blue,
+                    (int)IssuePriority.Hint => Color.LightSkyBlue1,
+                    _ => throw new Exception(),
+                };
+
+            this.CreateLabels();
         }
 
-        /// <inheritdoc />
-        public override Color GetColor()
+        /// <summary>
+        /// Creates labels for the issue.
+        /// </summary>
+        private void CreateLabels()
         {
-            return this.issue.Priority switch
+            var color = this.issue.Priority switch
             {
                 (int)IssuePriority.Error => Color.Red,
                 (int)IssuePriority.Warning => Color.Yellow,
-                (int)IssuePriority.Suggestion => Color.Blue,
-                (int)IssuePriority.Hint => Color.LightSkyBlue1,
+                (int)IssuePriority.Suggestion or (int)IssuePriority.Hint => Color.Blue,
                 _ => throw new Exception(),
             };
+
+            (var location, var length) = this.GetLocation(this.issue);
+            var label =
+                new Label(
+                    this.issue.AffectedFileRelativePath.FullPath,
+                    location,
+                    this.issue.Message(IssueCommentFormat.PlainText))
+                .WithColor(color);
+
+            if (length > 0)
+            {
+                label = label.WithLength(length);
+            }
+
+            this.Labels.Add(label);
         }
 
-        /// <inheritdoc />
-        public override string GetPrefix()
+        /// <summary>
+        /// Returns the diagnostic location of an issue.
+        /// </summary>
+        /// <param name="issue">Issue for which the location should be returned.</param>
+        /// <returns>Location for the diagnostic.</returns>
+        private (Location location, int lenght) GetLocation(IIssue issue)
         {
-            return this.issue.PriorityName;
+            // Errata currently doesn't support file or line level diagnostics.
+            if (!issue.Line.HasValue || !issue.Column.HasValue)
+            {
+                return default;
+            }
+
+            var location = new Location(issue.Line.Value, issue.Column.Value);
+
+            int lenght = 0;
+            if (issue.EndColumn.HasValue)
+            {
+                lenght = issue.EndColumn.Value - issue.Column.Value;
+            }
+
+            return (location, lenght);
         }
     }
 }
