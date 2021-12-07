5463bae17eb51e99eff645001808ab0f1ef798ed
Pascal Berger <pascal.berger@gmail.com>
2021-12-08 00:18:20 +0100

    (GH-433) Update jsPDF to 2.4.0 (#435)

--- src/Cake.Issues.Reporting.Generic/HtmlDxDataGridOption.cs
+++ src/Cake.Issues.Reporting.Generic/HtmlDxDataGridOption.cs
@@ -448,7 +448,7 @@
         /// <summary>
         /// Version of <see href="https://github.com/MrRio/jsPDF">jsPDF</see> which should be used.
         /// This version needs to match the version required by the selected <see cref="DevExtremeVersion"/>.
-        /// Default value is <c>2.3.1</c>.
+        /// Default value is <c>2.4.0</c>.
         /// </summary>
         JsPdfVersion,
 
