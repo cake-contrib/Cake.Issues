7bd3a05129e2a0b949d561c0c02101ec949bc617
Pascal Berger <pascal.berger@gmail.com>
2023-12-11 22:51:35 +0100

    Multi-Target .NET 6, .NET 7 & .NET 8 (#252)

--- src/Cake.Issues.EsLint/EsLintIssuesProvider.cs
+++ src/Cake.Issues.EsLint/EsLintIssuesProvider.cs
@@ -5,24 +5,17 @@
     /// <summary>
     /// Provider for issues reported by ESLint.
     /// </summary>
-    public class EsLintIssuesProvider : BaseMultiFormatIssueProvider<EsLintIssuesSettings, EsLintIssuesProvider>
+    /// <param name="log">The Cake log context.</param>
+    /// <param name="settings">Settings for reading the log file.</param>
+    public class EsLintIssuesProvider(ICakeLog log, EsLintIssuesSettings settings)
+        : BaseMultiFormatIssueProvider<EsLintIssuesSettings, EsLintIssuesProvider>(log, settings)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EsLintIssuesProvider"/> class.
-        /// </summary>
-        /// <param name="log">The Cake log context.</param>
-        /// <param name="settings">Settings for reading the log file.</param>
-        public EsLintIssuesProvider(ICakeLog log, EsLintIssuesSettings settings)
-            : base(log, settings)
-        {
-        }
-
         /// <summary>
         /// Gets the name of the ESLint issue provider.
         /// This name can be used to identify issues based on the <see cref="IIssue.ProviderType"/> property.
         /// </summary>
         public static string ProviderTypeName => typeof(EsLintIssuesProvider).FullName;
-        
+
         /// <inheritdoc />
         public override string ProviderName => "ESLint";
     }
