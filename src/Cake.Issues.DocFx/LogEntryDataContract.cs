575946be4e7e56616210ae938d76ecfcdaef7987
Pascal Berger <pascal.berger@swissgrc.com>
2023-12-26 01:45:41 +0100

    Remove unecessary suppresions

--- src/Cake.Issues.DocFx/LogEntryDataContract.cs
+++ src/Cake.Issues.DocFx/LogEntryDataContract.cs
@@ -5,9 +5,7 @@
 #pragma warning disable SA1310 // Field names must not contain underscore
 #pragma warning disable SA1401 // Fields must be private
 #pragma warning disable SA1307 // Accessible fields must begin with upper-case letter
-#pragma warning disable SA1402 // File may only contain a single class
 #pragma warning disable SA1600 // Elements must be documented
-#pragma warning disable SA1649 // File name must match first type name
 
     [DataContract]
     internal class LogEntryDataContract
@@ -31,7 +29,5 @@
 #pragma warning restore SA1310 // Field names must not contain underscore
 #pragma warning restore SA1401 // Fields must be private
 #pragma warning restore SA1307 // Accessible fields must begin with upper-case letter
-#pragma warning restore SA1402 // File may only contain a single class
 #pragma warning restore SA1600 // Elements must be documented
-#pragma warning restore SA1649 // File name must match first type name
 }
