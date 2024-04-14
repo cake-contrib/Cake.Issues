119b12286c9d6e982ce3c9298abf6fdb0ec813c6
Pascal Berger <pascal.berger@swissgrc.com>
2024-04-14 08:46:46 +0200

    Use primary constructor (IDE0290)

--- src/Cake.Issues.EsLint/BaseEsLintLogFileFormat.cs
+++ src/Cake.Issues.EsLint/BaseEsLintLogFileFormat.cs
@@ -5,15 +5,9 @@
     /// <summary>
     /// Base class for all ESLint log file format implementations.
     /// </summary>
-    public abstract class BaseEsLintLogFileFormat : BaseLogFileFormat<EsLintIssuesProvider, EsLintIssuesSettings>
+    /// <param name="log">The Cake log instance.</param>
+    public abstract class BaseEsLintLogFileFormat(ICakeLog log)
+        : BaseLogFileFormat<EsLintIssuesProvider, EsLintIssuesSettings>(log)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BaseEsLintLogFileFormat"/> class.
-        /// </summary>
-        /// <param name="log">The Cake log instance.</param>
-        protected BaseEsLintLogFileFormat(ICakeLog log)
-            : base(log)
-        {
-        }
     }
 }
--- src/Cake.Issues.PullRequests/BaseFilteringByModifiedFilesCapability.cs
+++ src/Cake.Issues.PullRequests/BaseFilteringByModifiedFilesCapability.cs
@@ -8,20 +8,12 @@
     /// Capability to filter issues to only those affecting files modified in the pull request.
     /// </summary>
     /// <typeparam name="T">Type of the pull request system to which this capability belongs.</typeparam>
-    public abstract class BaseFilteringByModifiedFilesCapability<T>
-        : BasePullRequestSystemCapability<T>, ISupportFilteringByModifiedFiles
+    /// <param name="log">The Cake log context.</param>
+    /// <param name="pullRequestSystem">Pull request system to which this capability belongs.</param>
+    public abstract class BaseFilteringByModifiedFilesCapability<T>(ICakeLog log, T pullRequestSystem)
+        : BasePullRequestSystemCapability<T>(log, pullRequestSystem), ISupportFilteringByModifiedFiles
         where T : class, IPullRequestSystem
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BaseFilteringByModifiedFilesCapability{T}"/> class.
-        /// </summary>
-        /// <param name="log">The Cake log context.</param>
-        /// <param name="pullRequestSystem">Pull request system to which this capability belongs.</param>
-        protected BaseFilteringByModifiedFilesCapability(ICakeLog log, T pullRequestSystem)
-            : base(log, pullRequestSystem)
-        {
-        }
-
         /// <inheritdoc/>
         public IEnumerable<FilePath> GetModifiedFilesInPullRequest()
         {
