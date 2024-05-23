1e1f9a6e1285f63efd94c097f7a856420091587c
Pascal Berger <pascal.berger@gmail.com>
2024-05-23 09:26:14 +0200

    Document default values (#626)

--- src/Cake.Issues.Reporting.Console/ConsoleIssueReportFormatSettings.cs
+++ src/Cake.Issues.Reporting.Console/ConsoleIssueReportFormatSettings.cs
@@ -20,18 +20,21 @@ public class ConsoleIssueReportFormatSettings
     /// <summary>
     /// Gets or sets a value indicating whether issues should be grouped by rule or if
     /// for every issue a separate diagnostic should be created.
+    /// Default value is <c>false</c>.
     /// </summary>
     public bool GroupByRule { get; set; }
 
     /// <summary>
     /// Gets or sets a value indicating whether a summary of issues by provider / run
     /// should be shown.
+    /// Default value is <c>false</c>.
     /// </summary>
     public bool ShowProviderSummary { get; set; }
 
     /// <summary>
     /// Gets or sets a value indicating whether a summary of issues by provider / run
     /// with the number of issues for each priority should be shown.
+    /// Default value is <c>false</c>.
     /// </summary>
     public bool ShowPrioritySummary { get; set; }
 }
