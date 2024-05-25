3dd21007ae947cf70b2950159e443bf93f117e68
Pascal Berger <pascal.berger@gmail.com>
2024-05-25 11:40:15 +0200

    Improve logging formatting (#632)

---
+++ src/Cake.Issues.Tests/StringExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Cake.Issues.Tests;
+
+public sealed class StringExtensionsTests
+{
+    public sealed class TheToStringWithNullMarkupMethod
+    {
+        [Fact]
+        public void Should_Return_NotSetString_When_Value_Is_Null()
+        {
+            // Given
+            object value = null;
+
+            // When
+            var result = value.ToStringWithNullMarkup();
+
+            // Then
+            result.ShouldBe("[italic grey]Not set[/]");
+        }
+
+        [Theory]
+        [InlineData("foo", "foo")]
+        [InlineData(1, "1")]
+        public void Should_Return_Value_ToString_When_Value_Is_Not_Null(object value, string expected)
+        {
+            // Given
+
+            // When
+            var result = value.ToStringWithNullMarkup();
+
+            // Then
+            result.ShouldBe(expected);
+        }
+    }
+}
---
+++ src/Cake.Issues/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Cake.Issues;
+
+/// <summary>
+/// Extension methods for <see cref="string"/>.
+/// </summary>
+public static class StringExtensions
+{
+    private const string NotSetString = "[italic grey]Not set[/]";
+
+    /// <summary>
+    /// Returns the string value or a <c>Not set</c>> markup suitable for Spectre.Console.
+    /// </summary>
+    /// <param name="value">String which should be returned.</param>
+    /// <returns>String value or a <c>Not set</c>> markup.</returns>
+    public static string ToStringWithNullMarkup(this object value) =>
+        value == null ? NotSetString : value.ToString();
+}
