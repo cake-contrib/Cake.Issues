9c1e3f90ffeab5b28b1de1beeb7d68746c62d6fd
Pascal Berger <pascal.berger@swissgrc.com>
2024-07-19 10:09:01 +0200

    Add option to configure if tool name or fix issue provider name is used

--- src/Cake.Issues.Sarif.Tests/SarifIssuesProviderFixture.cs
+++ src/Cake.Issues.Sarif.Tests/SarifIssuesProviderFixture.cs
@@ -3,5 +3,15 @@
 internal class SarifIssuesProviderFixture(string fileResourceName)
     : BaseConfigurableIssueProviderFixture<SarifIssuesProvider, SarifIssuesSettings>(fileResourceName)
 {
+    public bool UseToolNameAsIssueProviderName { get; set; } = true;
+
     protected override string FileResourceNamespace => "Cake.Issues.Sarif.Tests.Testfiles.";
+
+    protected override SarifIssuesSettings CreateIssueProviderSettings()
+    {
+        var settings = base.CreateIssueProviderSettings();
+        settings.UseToolNameAsIssueProviderName = this.UseToolNameAsIssueProviderName;
+        return settings;
+    }
+
 }
