472c4923fe6f640755966acd7822ae8251debe8b
Pascal Berger <pascal.berger@swissgrc.com>
2024-09-04 22:10:17 +0200

    Update jspdf-autotable to 3.8.3

--- src/Cake.Issues.Reporting.Generic/HtmlDxDataGridOption.cs
+++ src/Cake.Issues.Reporting.Generic/HtmlDxDataGridOption.cs
@@ -489,7 +489,7 @@ public enum HtmlDxDataGridOption
     /// <summary>
     /// Version of <see href="https://github.com/simonbengtsson/jsPDF-AutoTable">jsPDF-AutoTable plugin</see> which should be used.
     /// This version needs to match the version required by the selected <see cref="DevExtremeVersion"/>.
-    /// Default value is <c>3.8.2</c>.
+    /// Default value is <c>3.8.3</c>.
     /// </summary>
     JsPdfAutotableVersion,
 }
