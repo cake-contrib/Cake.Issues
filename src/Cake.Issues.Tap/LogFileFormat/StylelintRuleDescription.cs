f0d386bd00d573119e6a418d8138d3620ec77fbb
Pascal Berger <pascal.berger@gmail.com>
2025-01-12 16:20:20 +0100

    Add rule url resolver for Stylelint (#986)

---
+++ src/Cake.Issues.Tap/LogFileFormat/StylelintRuleDescription.cs
@@ -0,0 +1,12 @@
+﻿namespace Cake.Issues.Tap.LogFileFormat;
+
+/// <summary>
+/// Class describing rules appearing in TAP logs written by stylelint.
+/// </summary>
+public class StylelintRuleDescription : BaseRuleDescription
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether the rule is from a plugin or not.
+    /// </summary>
+    public bool IsPlugin { get; set; }
+}
