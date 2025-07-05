ff42503e36b0810d2f17fde22603c83dec8861b1
Pascal Berger <pascal.berger@gmail.com>
2025-07-05 13:41:14 +0200

    Update

--- src/Cake.Issues.Reporting.Generic/HtmlDxDataGridOption.cs
+++ src/Cake.Issues.Reporting.Generic/HtmlDxDataGridOption.cs
@@ -469,7 +469,7 @@ public enum HtmlDxDataGridOption
     /// <summary>
     /// Version of <see href="https://github.com/MrRio/jsPDF">jsPDF</see> which should be used.
     /// This version needs to match the version required by the selected <see cref="DevExtremeVersion"/>.
-    /// Default value is <c>2.5.2</c>.
+    /// Default value is <c>3.0.1</c>.
     /// </summary>
     JsPdfVersion,
 
