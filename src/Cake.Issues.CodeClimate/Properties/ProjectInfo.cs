3fd284e9c3d761ccaa4d0f76e5fc230d7f5ba833
Pascal Berger <pascal.berger@gmail.com>
2025-08-01 16:19:44 +0200

    Fix assembly properties

--- src/Cake.Issues.CodeClimate/Properties/ProjectInfo.cs
+++ src/Cake.Issues.CodeClimate/Properties/ProjectInfo.cs
@@ -1,15 +1,11 @@
+using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cake.Issues.CodeClimate")]
-[assembly: AssemblyDescription("Support for CodeClimate compatible files for the Cake.Issues Addin for Cake Build Automation System")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cake.Issues")]
-[assembly: AssemblyCopyright("Copyright © Cake Issues contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,3 +16,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b3f1a4be-7c7b-4a9e-8b7f-4e4e5c5b4d4e")]
+
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("Cake.Issues.CodeClimate.Tests")]
