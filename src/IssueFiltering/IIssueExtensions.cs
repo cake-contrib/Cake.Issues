c7e69fa1a93cb46b7fb9d782f2cbb239b4051240
Pascal Berger <pascal.berger@gmail.com>
2025-09-01 22:29:01 +0200

    Replace shraed class library with a shared project

---
+++ src/IssueFiltering/IIssueExtensions.cs
@@ -0,0 +1,30 @@
+namespace Cake.Issues.Shared;
+
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Extensions for <see cref="IIssue"/>s.
+/// </summary>
+internal static class IIssueExtensions
+{
+    /// <summary>
+    /// Sorts issues based on the following criteria:
+    /// <list type="number">
+    /// <description><see cref="IIssue.Priority"/> descending</description>
+    /// <description><see cref="IIssue.AffectedFileRelativePath"/> is null</description>
+    /// <description> The <see cref="IIssue.AffectedFileRelativePath"/> FullPath.</description>
+    /// </list>
+    /// </summary>
+    /// <param name="issues">Issues to be sorted.</param>
+    /// <returns>The sorted issues.</returns>
+    public static IEnumerable<IIssue> SortWithDefaultPrioritization(this IEnumerable<IIssue> issues)
+    {
+        issues.NotNull();
+
+        return issues
+            .OrderByDescending(x => x.Priority)
+            .ThenBy(x => x.AffectedFileRelativePath is null)
+            .ThenBy(x => x.AffectedFileRelativePath?.FullPath);
+    }
+}
